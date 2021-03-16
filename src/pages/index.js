ea48afddde83eb5114bfa31628bd21e96679f218
Nikita-Kukenko <kukenkonik@gmail.com>
2021-03-16 16:48:59 +0200

    create section Information, and section wipes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react"
 import Loader from "../Loader/Loader"
+import Information from "../atomic/organisms/Information/Information"
 import Technology from "../atomic/organisms/Technology/Technology"
 import Contacts from "../atomic/organisms/Contacts/Contacts"
 import reportWebVitals from "../reportWebVitals"
+import ScrollMagic from "scrollmagic"
 
 import Title from "../atomic/atoms/Title/Title"
 import Text from "../atomic/atoms/Text/Text"
@@ -11,6 +13,13 @@ import "../App.css"
 
 export default function Home() {
   const [loaderState, setLoaderState] = useState(true)
+  const [scrollMagic, setScrollMagic] = useState({
+    controller: new ScrollMagic.Controller({
+      globalSceneOptions: { triggerHook: "onLeave", duration: "200%" },
+    }),
+  })
+
+  const { controller } = scrollMagic
 
   useEffect(() => {
     window.onload = () => {
@@ -23,26 +32,44 @@ export default function Home() {
       bodyElement.style.overflow = "auto"
       setLoaderState(false)
     }, 5000)
+
+    const sections = document.querySelectorAll("section")
+    console.log(sections)
+
+    for (let i = 0; i < sections.length; i++) {
+      new ScrollMagic.Scene({
+        triggerElement: sections[i],
+      })
+        .setPin(sections[i], { pushFollowers: false })
+        .addIndicators() // add indicators (requires plugin)
+        .addTo(controller)
+    }
   }, [])
 
   return (
     <div className="A-Team">
       {loaderState && <Loader />}
       <div className="content">
-        <div className="hero-section">
-          <div className="name">A-Team</div>
-          <img src={HeroImage} className="hero-logo" alt="logo" />
-        </div>
-        <div className="middle-section">
-          <Text textClassName="line-1 anim-typewriter">
-            We can everything you want and do it good
-          </Text>
-          <Title size="1">
-            We are a small but super talented company with the high level IT
-            specialists.
-          </Title>
-        </div>
+        <section style={{ height: "100vh" }}>
+          <div className="hero-section">
+            <div className="name">A-Team</div>
+            <img src={HeroImage} className="hero-logo" alt="logo" />
+          </div>
+          <div className="middle-section">
+            <Text textClassName="line-1 anim-typewriter">
+              We can everything you want and do it good
+            </Text>
+            <Title size="1">
+              We are a small but super talented company with the high level IT
+              specialists.
+            </Title>
+          </div>
+        </section>
+
+        <Information />
+
         <Technology />
+
         <Contacts />
       </div>
     </div>
