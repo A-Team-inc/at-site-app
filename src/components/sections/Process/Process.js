cd4559c5916b3e235db67cc247b349eaae8ca1f2
AAPK-code <aapk.upwork@gmail.com>
2022-09-23 14:35:23 +0300

    add scroll on click to Process section

--- src/components/sections/Process/Process.js
+++ src/components/sections/Process/Process.js
@@ -9,13 +9,27 @@ const Process = () => {
   const [currentStep, setCurrentStep] = useState(data?.contentfulProcess.steps[0])
   const [currentIndex, setCurrentIndex] = useState(0)
 
+  const scrollContainer = useRef()
+  const stepRef = useRef()
+
   const handleClick = (step, index) => {
+    // scroll on click
+    const cardWidth = stepRef.current.clientWidth
+    const gapBetweenCards = 24
+    scrollContainer.current.scrollLeft = index * (cardWidth + gapBetweenCards)
     setCurrentStep(step)
     setCurrentIndex(index)
   }
 
-  const scrollContainer = useRef()
+  const keyDown = (event, step, index) => {
+    if (event.key === 'Enter') {
+      setCurrentStep(step)
+      setCurrentIndex(index)
+    }
+  }
+
   useEffect(() => {
+    // scroll with mouse wheel
     scrollContainer.current.addEventListener("wheel", event => {
       if (window.innerWidth < 1024) {
         event.preventDefault();
@@ -41,9 +55,11 @@ const Process = () => {
           {data?.contentfulProcess.steps.map((step, index) => (
             <Step
               step={step}
-              handleClick={() => handleClick(step, index)}
+              handleClick={handleClick}
               isActive={index === currentIndex}
-              index={`0${index + 1}`}
+              index={index}
+              stepRef={stepRef}
+              keyDown={keyDown}
               key={index}
             />
           ))}
@@ -74,13 +90,15 @@ const Process = () => {
 }
 
 
-const Step = ({step, handleClick, isActive, index}) => (
+const Step = ({ step, handleClick, isActive, index, stepRef, keyDown }) => (
   <div
     className={`process-step tabIndexItem ${isActive ? 'process-step--active' : ''}`}
-    onClick={handleClick}
+    onClick={() => handleClick(step, index)}
+    onKeyDown={(event) => keyDown(event, step, index)}
+    ref={stepRef}
     tabIndex="0"
   >
-    <div className="process-step__number">{index}</div>
+    <div className="process-step__number">{`0${index + 1}`}</div>
     <h6 className="process-step__title title">{step.title}</h6>
     <p className="process-step__content">{step.content.content}</p>
   </div>
