c01682635c9357f61d366b624a5f321113bfa766
aapk <aapk.upwork@gmail.com>
2023-01-29 18:15:38 +0100

    Fix process step

--- src/graphql/process.js
+++ src/graphql/process.js
@@ -9,9 +9,6 @@ const useProcessQuery = () => {
         cta,
         steps {
           title,
-          content {
-            content
-          },
           description {
             description
           }
